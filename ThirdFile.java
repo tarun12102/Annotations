fa5b2d2376f8a8129305f7897dcba411aaf92302
tarun12102 <61898012+tarun12102@users.noreply.github.com>
2023-10-21 23:48:57 +0530

    Update ThirdFile.java

--- ThirdFile.java
+++ ThirdFile.java
@@ -1,6 +1,6 @@
 public class ThirdFile {
 
-    @MyAnnotation(name = "ThirdFile",val = "var1")
+    @BatchuAnnotation(name = "ThirdFile",val = "var1")
     int var1;
 
     @MyAnnotation(name = "ThirdFile",val = "var2")
